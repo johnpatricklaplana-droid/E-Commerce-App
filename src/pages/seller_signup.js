49224ae1a6d6d087388d3dcc1ac67c6a6945cba8
John Patrick Laplana <johnpatricklaplana@gmail.com>
2026-03-18 22:50:49 +0800

    refactor: centralize popup toggle logic and encapsulate image preview with IIFE

--- src/pages/seller_signup.js
+++ src/pages/seller_signup.js
@@ -1,20 +1,24 @@
-const input = document.getElementById("imageUpload");
-const preview = document.getElementById("preview");
-const placeholder = document.getElementById("placeholder");
+(() => {
 
-input.addEventListener("change", function () {
-    const file = this.files[0];
+    const input = document.getElementById("imageUpload");
+    const preview = document.getElementById("preview");
+    const placeholder = document.getElementById("placeholder");
 
-    if(file) {
-        const reader = new FileReader();
+    input.addEventListener("change", function () {
+        const file = this.files[0];
 
-        reader.onload = function (e) {
-            preview.src = e.target.result;
-            preview.classList.remove("hidden");
-            placeholder.classList.add("hidden");
+        if (file) {
+            const reader = new FileReader(); 
+
+            reader.onload = function (e) {
+                preview.src = e.target.result;
+                preview.classList.remove("hidden");
+                placeholder.classList.add("hidden");
+            }
+
+            reader.readAsDataURL(file);
         }
 
-        reader.readAsDataURL(file);
-    }
+    });
 
-});
+}) ();
